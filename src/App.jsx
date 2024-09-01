5d03b03e547323d669c2e2aebe7170845d8dac57
Shivam4355 <shivampatankar1818@gmail.com>
2024-09-01 10:56:26 +0530

    Food Recipe App

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Mainpage from "./Component/Mainpage";
+import { Route, Routes } from "react-router-dom";
+import Mealinfo from "./Component/Mealinfo";
+
+function App() {
+  return (
+    //<Mainpage />
+    <Routes>
+      <Route path="/" element={<Mainpage />} />
+      <Route path="/:mealid" element={<Mealinfo />} />
+    </Routes>
+  );
+}
+
+export default App;
